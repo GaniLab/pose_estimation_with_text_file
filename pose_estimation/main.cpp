d595fb4cdd4469e65f23d62eb461cc9bf2be20e0
musfiqin gani <choirul.gani@student.kit.edu>
2019-06-11 09:48:15 +0200

    Revised project2

--- pose_estimation/main.cpp
+++ pose_estimation/main.cpp
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <iomanip>
 
+// pose estimation with PnP problem approach
 #include <opencv2/highgui.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/calib3d.hpp>
@@ -18,39 +19,34 @@
 using namespace std;
 using namespace cv;
 
-//2D image points extractor from text file data.
-vector<Point2d> imagePoints(ifstream& csvImage)
-{
-	vector<Point2d> imagePoints;
+// Function prototyping
 
-	double u, v;
+// 2D image points extractor from text file data.
+vector<Point2d> imagePoints(ifstream& csvImage);
 
-	while (!csvImage.eof())
-	{
-		csvImage >> u >> v;
-		imagePoints.push_back(Point2d(u, v));
-	}
+// 3D model object points extractor from text file data.
+vector<Point3d> modelPoints(ifstream& csvModel);
 
-	
-	return imagePoints;
-}
+// printing rotation vectors into text file
+void printRvecs(ofstream& csvRvecs, Mat& rvecs);
 
-//3D model object points extractor from text file data.
-vector<Point3d> modelPoints(ifstream& csvModel)
-{
-	vector<Point3d> modelPoints;
+// printing translation vectors into text file
+void printTvecs(ofstream& csvTvecs, Mat& tvecs);
 
-	double k, l, m;
+// printing 3x3 rotation matrix in rodrigues form into text file
+void printRodriguesRotation(ofstream& csvR, Mat& rvec, Mat& R);
 
-	while (!csvModel.eof())
-	{
-		csvModel >> k >> l >> m;
-		modelPoints.push_back(Point3d(k, l, m));
-	}
+// create mat object for extrinsic matrix
+Mat extrinsic_matrix(Mat& R, Mat& tvec);
 
+// printing extrinsic matrix into text file
+void print_extrinsic(ofstream& csvExtrinsicMatrix, Mat& E);
 
-	return modelPoints;
-}
+// create mat object for projection matrix
+Mat projection_matrix(Mat& camera_matrix, Mat& extrinstic_matrix);
+
+// printing projection matrix into text file
+void print_projection_matrix(ofstream& csvProj, Mat& P);
 
 
 int main(int argc, char **argv)
@@ -77,9 +73,9 @@ int main(int argc, char **argv)
 
 	Mat camera_matrix = Mat(3, 3, CV_64F, arrayIntrinsic);
 
-	csvIntrinsic.close();
-
 	cout << "Camera Matrix " << endl << camera_matrix << endl;
+
+	csvIntrinsic.close();
 
 	cout << endl;
 
@@ -109,16 +105,18 @@ int main(int argc, char **argv)
 	// 2D image points input in pixel
 	ifstream im1;
 
-	im1.open("../input_data/undistorted/y_axis/single_Y-500.txt");
-	
-	vector<Point2d> image_points = imagePoints(im1);
+	im1.open("../input_data/undistorted/x_axis/single_X-100.txt");
+
+	vector<Point2d> image1 = imagePoints(im1);
 
 	im1.close();
 
-	cout << " image points : " << endl << image_points << endl;
+	cout << " image points : " << endl << image1 << endl;
 
 	cout << endl;
-	
+
+
+
 
 	// 3D model points input in mm
 	ifstream mod1;
@@ -126,7 +124,7 @@ int main(int argc, char **argv)
 	mod1.open("../input_data/model_points_bundle_All.txt");
 
 	vector<Point3d> model_points = modelPoints(mod1);
-	
+
 	mod1.close();
 
 	cout << " model points : " << endl << model_points << endl;
@@ -181,44 +179,120 @@ int main(int argc, char **argv)
 	cout << endl;
 
 	// initialize output file stream for pose estimation result
-	ofstream csvRvecs("../output_data/rvecs/Rotation_Vectors.txt");
 
-	ofstream csvTvecs("../output_data/tvecs/Translation_Vectors.txt");
+
 
 	// Function to solve the pose
-	solvePnP(model_points,			// a set of 3D object points of type vector <Point3d>
-			image_points,			// a set of image points of type vector <Point2d>
-			camera_matrix,			// intrinsic camera parameters
-			dist_coeffs,			// distortion coefficients
-			rvec,					// output rotation in euler angles form
-			tvec,					// output translation
-			0,						// no initial rotation and translation
-			SOLVEPNP_ITERATIVE);	// use Levenberg-Marquardt algorithm to optimize pose estimation
+	bool pose_image_1 = solvePnP(model_points, image1, camera_matrix, dist_coeffs, rvec, tvec, 0, SOLVEPNP_ITERATIVE);
 
 	// formating output rotation vector from radian to degree
-	double phi = 3.14159;
+	ofstream csvRvecs("../output_data/rvecs/Rotation_Vectors.txt");
 
-	Mat newRvec = rvec * (1);
-
-	cout << "Rotation Vector (in degree):  " << endl << setprecision(17) << (newRvec) * 180 / phi << "\n" << endl;
-
-	csvRvecs << setprecision(17) << newRvec.at<double>(0) * 180 / phi << " " << newRvec.at<double>(1) * 180 / phi << " " << newRvec.at<double>(2) * 180 / phi << endl;
+	printRvecs(csvRvecs, rvec);
 
 	csvRvecs.close();
 
 	// formating output translation into high precision decimal points
-	Mat newTvec = tvec;
+	ofstream csvTvecs("../output_data/tvecs/Translation_Vectors.txt");
 
-	cout << "Translation Vector (in mm):" << endl << setprecision(17) << newTvec << "\n" << endl;
+	printTvecs(csvTvecs, tvec);
 
-	csvTvecs << setprecision(17) << newTvec.at<double>(0) << " " << newTvec.at<double>(1) << " " << newTvec.at<double>(2) << endl;
-	
 	csvTvecs.close();
 
 	// finding 3x3 rotations in rodrigues form and outputing the result into .txt file format
+	Mat R(3, 3, CV_64F);
+
 	ofstream csvR("../output_data/rot3x3_rodrigues/Rotation3x3.txt");
 
-	Mat R(3, 3, CV_64F);
+	printRodriguesRotation(csvR, rvec, R);
+
+	csvR.close();
+
+	cout << endl;
+
+	// combining 3x3 rotations and translations to make extrinsic matrix output in .txt file format
+	ofstream csvExtrinsicMatrix("../output_data/extrinsic_matrix/Extrinsic_Matrix.txt");
+
+	Mat E = extrinsic_matrix(R, tvec);
+
+	print_extrinsic(csvExtrinsicMatrix, E);
+
+	csvExtrinsicMatrix.close();
+
+	cout << endl;
+
+	// initialize projection identity matrix
+	Mat P = projection_matrix(camera_matrix, E);
+
+	// finding projection matrix and outputting the result in .txt file format
+	ofstream csvProj("../output_data/projection_matrix/Projection_Matrix.txt");
+
+	print_projection_matrix(csvProj, P);
+
+	csvProj.close();
+
+	cout << endl;
+
+	system("pause");
+	return 0;
+
+}
+
+//2D image points extractor from text file data.
+vector<Point2d> imagePoints(ifstream& csvImage)
+{
+	vector<Point2d> imagePoints;
+
+	double u, v;
+
+	while (!csvImage.eof())
+	{
+		csvImage >> u >> v;
+		imagePoints.push_back(Point2d(u, v));
+	}
+
+
+	return imagePoints;
+}
+
+//3D model object points extractor from text file data.
+vector<Point3d> modelPoints(ifstream& csvModel)
+{
+	vector<Point3d> modelPoints;
+
+	double k, l, m;
+
+	while (!csvModel.eof())
+	{
+		csvModel >> k >> l >> m;
+		modelPoints.push_back(Point3d(k, l, m));
+	}
+
+
+	return modelPoints;
+}
+
+// printing rotation vectors into text file
+void printRvecs(ofstream& csvRvecs, Mat& rvecs)
+{
+	double phi = 3.14159;
+
+	cout << "Rotation Vector (in degree):  " << endl << setprecision(17) << (rvecs) * 180 / phi << "\n" << endl;
+
+	csvRvecs << setprecision(17) << rvecs.at<double>(0) * 180 / phi << " " << rvecs.at<double>(1) * 180 / phi << " " << rvecs.at<double>(2) * 180 / phi << endl;
+}
+
+// printing translation vectors into text file
+void printTvecs(ofstream& csvTvecs, Mat& tvecs)
+{
+	cout << "Translation Vector (in mm):" << endl << setprecision(17) << tvecs << "\n" << endl;
+
+	csvTvecs << setprecision(17) << tvecs.at<double>(0) << " " << tvecs.at<double>(1) << " " << tvecs.at<double>(2) << endl;
+}
+
+// printing 3x3 rotation matrix in rodrigues form into text file
+void printRodriguesRotation(ofstream& csvR, Mat& rvec, Mat& R)
+{
 
 	Rodrigues(rvec, R);
 
@@ -228,52 +302,48 @@ int main(int argc, char **argv)
 	csvR << setprecision(17) << R.at<double>(1, 0) << " " << R.at<double>(1, 1) << " " << R.at<double>(1, 2) << endl;
 	csvR << setprecision(17) << R.at<double>(2, 0) << " " << R.at<double>(2, 1) << " " << R.at<double>(2, 2) << endl;
 
-	csvR.close();
+}
 
-	cout << endl;
-
-	// combining 3x3 rotations and translations to make extrinsic matrix output in .txt file format
-	ofstream csvExtrinsicMatrix("../output_data/extrinsic_matrix/Extrinsic_Matrix.txt");
-
-	Mat E = (Mat_<double>(4, 4) << R.at<double>(0, 0), R.at<double>(0, 1), R.at<double>(0, 2), newTvec.at<double>(0),
-		R.at<double>(1, 0), R.at<double>(1, 1), R.at<double>(1, 2), newTvec.at<double>(1),
-		R.at<double>(2, 0), R.at<double>(2, 1), R.at<double>(2, 2), newTvec.at<double>(2),
+// create mat object for extrinsic matrix
+Mat extrinsic_matrix(Mat& R, Mat& tvec)
+{
+	Mat E = (Mat_<double>(4, 4) << R.at<double>(0, 0), R.at<double>(0, 1), R.at<double>(0, 2), tvec.at<double>(0),
+		R.at<double>(1, 0), R.at<double>(1, 1), R.at<double>(1, 2), tvec.at<double>(1),
+		R.at<double>(2, 0), R.at<double>(2, 1), R.at<double>(2, 2), tvec.at<double>(2),
 		0, 0, 0, 1);
 
+	return E;
+}
+
+// printing extrinsic matrix into text file
+void print_extrinsic(ofstream& csvExtrinsicMatrix, Mat& E)
+{
 	cout << "Extrinsic matrix is:  \n" << setprecision(17) << E << endl;
 
 	csvExtrinsicMatrix << setprecision(17) << E.at<double>(0, 0) << " " << E.at<double>(0, 1) << " " << E.at<double>(0, 2) << " " << E.at<double>(0, 3) << endl;
 	csvExtrinsicMatrix << setprecision(17) << E.at<double>(1, 0) << " " << E.at<double>(1, 1) << " " << E.at<double>(1, 2) << " " << E.at<double>(1, 3) << endl;
 	csvExtrinsicMatrix << setprecision(17) << E.at<double>(2, 0) << " " << E.at<double>(2, 1) << " " << E.at<double>(2, 2) << " " << E.at<double>(2, 3) << endl;
 	csvExtrinsicMatrix << setprecision(17) << E.at<double>(3, 0) << " " << E.at<double>(3, 1) << " " << E.at<double>(3, 2) << " " << E.at<double>(3, 3) << endl;
+}
 
-	csvExtrinsicMatrix.close();
-
-	cout << endl;
-
-	// initialize projection identity matrix
-	Mat ProjIdentity = (Mat_<double>(3, 4) << 
-		1, 0, 0, 0,
+// create mat object for projection matrix
+Mat projection_matrix(Mat& camera_matrix, Mat& extrinstic_matrix)
+{
+	Mat ProjIdentity = (Mat_<double>(3, 4) << 1, 0, 0, 0,
 		0, 1, 0, 0,
 		0, 0, 1, 0);
 
-	 
-	// finding projection matrix and outputting the result in .txt file format
-	ofstream csvProj("../output_data/projection_matrix/Projection_Matrix.txt");
+	Mat P = camera_matrix * ProjIdentity * extrinstic_matrix;
 
-	Mat P = camera_matrix * ProjIdentity * E;
+	return P;
+}
 
+// printing projection matrix into text file
+void print_projection_matrix(ofstream& csvProj, Mat& P)
+{
 	cout << "The projection matrix is: \n" << setprecision(17) << P << endl;
 
 	csvProj << setprecision(17) << P.at<double>(0, 0) << " " << P.at<double>(0, 1) << " " << P.at<double>(0, 2) << " " << P.at<double>(0, 3) << endl;
 	csvProj << setprecision(17) << P.at<double>(1, 0) << " " << P.at<double>(1, 1) << " " << P.at<double>(1, 2) << " " << P.at<double>(1, 3) << endl;
 	csvProj << setprecision(17) << P.at<double>(2, 0) << " " << P.at<double>(2, 1) << " " << P.at<double>(2, 2) << " " << P.at<double>(2, 3) << endl;
-
-	csvProj.close();
-
-	cout << endl;
-
-	system("pause");
-	return 0;
-
 }
